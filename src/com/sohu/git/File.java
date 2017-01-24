c2552cafca64ed1f2f1976bd8d5b28921618a4fe
wb-qiujuli <3226607079@qq.com>
2017-01-24 15:56:28 +0800

    print "Hello Word!" 注意此时，仅是提交在本地git，还需要push才能提交到github中

---
+++ src/com/sohu/git/File.java
@@ -0,0 +1,14 @@
+package com.sohu.git;
+
+public class File {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Word");
+
+	}
+
+}
