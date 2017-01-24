1e816e08ee8b1ef66c1e15fedadb8e8a8f9fe84d
wb-qiujuli <3226607079@qq.com>
2017-01-24 16:03:45 +0800

    提交到本地，第二次

--- src/com/sohu/git/File.java
+++ src/com/sohu/git/File.java
@@ -8,6 +8,9 @@ public class File {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Word");
+		System.out.println("1234");
+		
+		
 
 	}
 
